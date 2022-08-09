bdd01f18ff7299b638807b181d61f415f3ae7b4f
patrickfarrell <patrickfarrell@mac143901.local.gmcc.grange.local>
2022-08-09 18:24:56 -0400

    Initial test setup

--- src/main/java/com/example/vendingmachine/VendingMachine.java
+++ src/main/java/com/example/vendingmachine/VendingMachine.java
@@ -1,5 +1,10 @@
 package com.example.vendingmachine;
 
+/**
+ * This class represents a physical Vending Machine
+ * 
+ * @author Patrick Farrell
+ */
 public class VendingMachine {
 	
 }
--- src/test/java/com/example/vendingmachine/VendingMachineTest.java
+++ src/test/java/com/example/vendingmachine/VendingMachineTest.java
@@ -4,6 +4,12 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 import org.junit.jupiter.api.Test;
 
+/**
+ * This class ensures that the VendingMachine is working properly
+ * based on the stated business or technical requirements
+ * 
+ * @author Patrick Farrell
+ */
 class VendingMachineTest {
 	
 	@Test
